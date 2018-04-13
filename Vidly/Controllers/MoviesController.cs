2832ef3a0e5ffc5caf621c8fa5898cc88f51d2d2
Ricardo Nunes <ricardo.nunes.isla@gmail.com>
2018-04-13 17:23:30 +0100

    Added the css file. Changed the BundleConfig in order to use the new css bootstrap file. Added the MoviesController. Added the Movie model class. Added the Repository that have the methods do feed the controller.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private VidlyRepository _repo;
+
+        public MoviesController()
+        {
+            _repo = new VidlyRepository();
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            List<Movie> movies = _repo.GetMovies().ToList();
+
+            return View(movies);
+        }
+    }
+}
---
+++ Vidly/Models/VidlyRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class VidlyRepository
+    {
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            Movie Shrek = new Movie()
+            {
+                Id = 1,
+                Name = "Shrek"
+            };
+
+            List<Movie> list = new List<Movie>
+            {
+                Shrek
+            };
+
+            return list;
+        }
+
+
+    }
+}
